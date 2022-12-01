fe13761f1b7b552ea60bbe72840b1000fc691dbd
Uriel Guadalupe Salazar Pérez <uri.salazarperez@gmail.com>
2022-11-30 19:41:28 -0600

    Practicas de la materia de Lenguajes y Automatas 2

---
+++ 18070532_01.js
@@ -0,0 +1,7 @@
+let nombre='          Uriel Guadalupe               '
+console.log("uso de trim");
+console.log(nombre.trim());
+console.log("uso de trim start");
+console.log(nombre.trimStart());
+console.log("uso de trim end:");
+console.log(nombre.trimEnd());
---
+++ 18070532_03.js
@@ -0,0 +1,17 @@
+const str1='el, dia, de, hoy, mexico, fue, eliminado, de la copa, del, mundo,';
+console.log("la frase con comas");
+console.log("frase: ",str1)
+const words1=str1.split(',');
+console.log(words1[0]);
+console.log(words1[1]);
+console.log(words1[2]);
+console.log(words1[3]);
+console.log(words1[4]);
+console.log(words1[5]);
+console.log(words1[6]);
+console.log(words1[7]);
+console.log(words1[8]);
+console.log(words1[9]);
+console.log(words1[10]);
+console.log(words1[11]);
+console.log(words1.length);
